2bfc2534cd4e820c5322c25a0ba2aba7cbc00f28
ramonnieto <rnieto@quonext.com>
2019-11-11 10:02:21 +0100

    primer commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,14 @@ module.exports = {
     'eslint:recommended'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV ===  'off',
+         "no-restricted-syntax": [
+                "error",
+                {
+                    "selector": "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+                    "message": "Unexpected property on console object was called"
+                }
+            ]
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
       "vue/no-use-v-if-with-v-for": "off"
   
